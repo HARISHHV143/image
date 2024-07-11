a8c5b0901335fce47295b31c64f116594e892007
HARISHHV143 <pvharish2019@gmail.com>
2024-07-11 12:07:25 +0530

    Add files via upload

---
+++ colorimage.js
@@ -0,0 +1,12 @@
+const imageSources = [
+    '15.jpg','2.jpg','3.jpg','4.jpg','5.jpg','12.jpg','7.jpg'
+    ,'10.jpg','9.jpg','8.jpg','6.jpg'
+];
+
+function changeBackgroundImage() {
+    const randomIndex = Math.floor(Math.random() * imageSources.length);
+    document.body.style.backgroundImage = `url(${imageSources[randomIndex]})`;
+}
+
+// Change the background image every 5 seconds (1000 milliseconds)
+setInterval(changeBackgroundImage, 1000);
